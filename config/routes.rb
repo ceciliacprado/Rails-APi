1d29197c7791f20f2e0c9798b72b0e493c7453f0
Cecilia <cecilia.prado@diprotec.com.br>
2025-01-03 09:44:50 -0300

    add namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
+  namespace :api, defaults: {format: :json}, 
+                             constants: {subdomain: 'api'}, path: '/' do
+  end 
+
 end
